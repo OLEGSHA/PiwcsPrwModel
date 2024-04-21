686f4943b0b82bae18edf6eefcdf91f505faebfe
Roman Lipov <lipov.ra@phystech.edu>
2024-04-22 00:09:49 +0300

    Begin implementing `isCorrect`
    - Begin implementing `isCorrect(const Model &model)`

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -22,4 +22,102 @@ bool isComplete(const Model &model) {
     return true;
 }
 
+bool isCorrect(const Model &model){
+
+
+
+	// Incomplete model is not correct
+	if (!isComplete(model)){
+		return false;
+	}
+
+	for (auto &[id, node]: model.nodes()){
+
+		bool m_trackCorrect = [&](SlotId start, SlotId end){
+
+			// Connected sections must have same directionality
+			if (model.section(node.section(start))->dir() != model.section(node.section(end))->dir()){
+				// Otherwise this is certainly a dead end
+				return false;
+			}
+
+			// Bidirectional track inside node is correct
+			if (model.section(node.section(start))->dir() == Section::AllowedTravel::BIDIR){
+				return true;
+			}
+
+			// Correct directional track must connect different slots of sections
+			if (model.section(node.section(0))->start() == id && model.section(node.section(1))->end() == id){
+				return true;
+			}
+			if (model.section(node.section(0))->end() == id && model.section(node.section(1))->start() == id){
+				return true;
+			}
+
+			return false;
+		};
+
+		// Check if node is inside forbidden region
+		bool isInForbidden = true;
+		for (SlotId slot=0; slot<=node.sectionCount(); ++slot) {
+			// If section in `slot` is not forbidden `node` is not in forbidden region
+			if (!(model.section(node.section(slot))->allowsTravel())){
+				isInForbidden = false;
+				break;
+			}
+		}
+
+		// Forbidden node is always locally correct
+		if (isInForbidden){
+			continue;
+		}
+
+		switch (node.type()) {
+			case END:
+				// Single slot of end must be connected to bidir. section
+				if (model.section(node.section(0))->isBidir()) {
+					continue;
+				}
+
+				return false;
+
+			case THRU:
+				// Check correctness of single track
+
+				if(m_trackCorrect(0, 1)){
+					continue;
+				}
+
+				return false;
+
+			case CROSSING:
+				// Check correctness of both tracks
+				if(m_trackCorrect(0, 1) && m_trackCorrect(2, 3)){
+					continue;
+				}
+
+				return false;
+
+			case MANUAL:
+				// Check correctness of common->straight track
+				if(!m_trackCorrect(COMMON, STRAIGHT)){
+					return false;
+				}
+
+				// Check that diverging connected to forbidden section
+				if(!model.section(node.section(DIVERGING))->allowsTravel()){
+					continue;
+				}
+				return false;
+
+			default:
+
+				return false;
+
+		}
+
+
+	}
+}
+
 } // namespace piwcs::prw
