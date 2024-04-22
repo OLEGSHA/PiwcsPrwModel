8a0f5ebcf9642f3dd7900f1bf767bf48901a5b95
Roman Lipov <lipov.ra@phystech.edu>
2024-04-22 19:14:41 +0300

    Implemented `isCorrect`
    - Implemented `isCorrect(const Model &model)`

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+
 #include <piwcsprwmodel/algorithms.h>
 
 namespace piwcs::prw {
@@ -24,39 +26,47 @@ bool isComplete(const Model &model) {
 
 bool isCorrect(const Model &model){
 
-
-
 	// Incomplete model is not correct
 	if (!isComplete(model)){
 		return false;
 	}
 
+	auto isInward = std::vector<bool>(Node::MAX_SLOTS, false);
+	auto isOutward = std::vector<bool>(Node::MAX_SLOTS, false);
+
 	for (auto &[id, node]: model.nodes()){
 
-		bool m_trackCorrect = [&](SlotId start, SlotId end){
 
-			// Connected sections must have same directionality
-			if (model.section(node.section(start))->dir() != model.section(node.section(end))->dir()){
-				// Otherwise this is certainly a dead end
+		/*
+		bool m_trackDirCorrect = [&](SlotId from, SlotId to){
+			// Correct directional track must connect two unidirectional sections
+			if (
+				!(model.section(node.section(from))->dir() == Section::AllowedTravel::UNIDIR) ||
+				!(model.section(node.section(from))->dir() == Section::AllowedTravel::UNIDIR)
+			){
 				return false;
 			}
-
-			// Bidirectional track inside node is correct
-			if (model.section(node.section(start))->dir() == Section::AllowedTravel::BIDIR){
-				return true;
-			}
-
 			// Correct directional track must connect different slots of sections
-			if (model.section(node.section(0))->start() == id && model.section(node.section(1))->end() == id){
-				return true;
-			}
-			if (model.section(node.section(0))->end() == id && model.section(node.section(1))->start() == id){
+			if (model.section(node.section(from))->end() == id && model.section(node.section(to))->start() == id){
 				return true;
 			}
 
 			return false;
 		};
 
+		bool m_trackBidirCorrect = [&](SlotId from, SlotId to){
+			// Correct bidirectional track must connect two bidirectional sections
+			if (
+				!(model.section(node.section(from))->dir() == Section::AllowedTravel::BIDIR) ||
+				!(model.section(node.section(to))->dir() == Section::AllowedTravel::BIDIR)
+			){
+				return false;
+			}
+
+			return true;
+		};
+		*/
+		/*
 		// Check if node is inside forbidden region
 		bool isInForbidden = true;
 		for (SlotId slot=0; slot<=node.sectionCount(); ++slot) {
@@ -71,16 +81,19 @@ bool isCorrect(const Model &model){
 		if (isInForbidden){
 			continue;
 		}
-
+		*/
 		switch (node.type()) {
 			case END:
-				// Single slot of end must be connected to bidir. section
-				if (model.section(node.section(0))->isBidir()) {
+				// Single slot of end must be connected to bidir. section or forbidden section
+				if (
+						model.section(node.section(0))->dir()==Section::AllowedTravel::NONE ||
+						model.section(node.section(0))->dir()==Section::AllowedTravel::BIDIR
+					) {
 					continue;
 				}
 
 				return false;
-
+			/*
 			case THRU:
 				// Check correctness of single track
 
@@ -109,15 +122,78 @@ bool isCorrect(const Model &model){
 					continue;
 				}
 				return false;
-
+			*/
 			default:
+				// Set outward/inward flags
+				for(SlotId start = 0; start<=node.sectionCount(); ++start){
+					for(SlotId end = 0; end<=node.sectionCount(); ++end){
+						if(
+							node.couldTraverse(start, end) &&
+							(
+								model.section(node.section(start))->isBidir() ||
+								(
+									model.section(node.section(start))->isUnidir() &&
+									model.section(node.section(start))->end() == id
+								)
+							)
+						){
+							isInward[start] = true;
+							isOutward[end] = false;
+						} // if
+					} // for end
+				} // for start
 
-				return false;
+				// Check slot correctness
+				for(SlotId slot = 0; slot<=node.sectionCount(); ++slot){
+					const Section* section = model.section(node.section(slot));
 
-		}
+					// Bidirectional slot
+					if(isInward[slot] && isOutward[slot]){
+						if (!section->isBidir()){
+							return false;
+						}
+					}
+
+					// None slot
+					if(!isInward[slot] && !isOutward[slot]) {
+						if(section->allowsTravel()){
+							return false;
+						}
+					}
+
+					// Unidirectional slot
+					if(!section->isUnidir()){
+						return false;
+					}
+
+					if(
+						isInward[slot] &&
+						!(section->end() == id)
+						){
+						return false;
+					}
+
+					if(
+						isOutward[slot] &&
+						!(section->start() == id)
+						){
+						return false;
+					}
+				}
+
+				// Reset outward/inward flags
+				for(SlotId slot = 0; slot<=node.sectionCount(); ++slot){
+					isInward[slot] = false;
+					isOutward[slot] = false;
+				}
+
+		} // switch(node.type())
 
 
-	}
+	} // for nodes
+
+	return true;
+
 }
 
 } // namespace piwcs::prw
