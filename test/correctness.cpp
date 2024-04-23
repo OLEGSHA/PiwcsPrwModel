7573c29624aa46efecad10863c648aeebc5db46b
Roman Lipov <lipov.ra@phystech.edu>
2024-04-23 23:28:04 +0300

    Added basic tests for `isCorrect`
    - Added basic tests for `isCorrect(const Model&)`
    - Added test sources to `tests` target

---
+++ test/correctness.cpp
@@ -0,0 +1,36 @@
+#include<exception>
+#include <gtest/gtest.h>
+
+#include <piwcsprwmodel.h>
+
+using namespace piwcs::prw;
+
+TEST(Correctness, Basic) {
+	Model model;
+    model.newNode(THRU, "n1");
+    model.newNode(THRU, "n2");
+    model.newSection("s1", Section::AllowedTravel::BIDIR, nullptr);
+    model.newSection("s2", Section::AllowedTravel::BIDIR, nullptr);
+    model.link("s1", "n1", 0, "n2", 0);
+    model.link("s2", "n1", 1, "n2", 1);
+
+    ASSERT_TRUE(isCorrect(model));
+
+}
+
+TEST(Correctness, EmptyIsCorrect){
+
+	ASSERT_TRUE(isCorrect(Model()));
+
+}
+
+TEST(Correctness, IncompleteIsIncorrect){
+	Model model;
+    model.newNode(THRU, "n1");
+    model.newNode(THRU, "n2");
+    model.newSection("s1", Section::AllowedTravel::BIDIR, nullptr);
+    model.newSection("s2", Section::AllowedTravel::BIDIR, nullptr);
+
+    ASSERT_FALSE(isComplete(model));
+
+}
