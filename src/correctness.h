537a6a5a98177040cdd047cf470162546280aa7d
Roman Lipov <lipov.ra@phystech.edu>
2024-04-25 01:57:17 +0300

    Changes proposed from https://github.com/OLEGSHA/PiwcsPrwModel/pull/1#discussion_r1577679369
    - Rename `isLocCorrect(const Model& model, IdRef id)` to `isLocallyCorrect(const Model& model, IdRef id`
    - Move `isLocallyCorrect` and `isCorrect` to correctness.cpp
    - Introduce private header for purpose of linking test executable against `isLocallyCorrect`

---
+++ src/correctness.h
@@ -0,0 +1,12 @@
+#ifndef CORRECTNESS_H
+#define CORRECTNESS_H
+
+#include <piwcsprwmodel/algorithms.h>
+
+namespace piwcs::prw {
+
+bool isLocallyCorrect(const Model& model, IdRef id);
+
+} // namespace piwcs::prw
+
+#endif // CORRECTNESS_H
