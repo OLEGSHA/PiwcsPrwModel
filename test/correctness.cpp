0ccf8cd6827c7677c52f30222378d123d5517309
Roman Lipov <lipov.ra@phystech.edu>
2024-04-25 02:39:56 +0300

    Add basic tests for `isLocallyCorrect`

--- test/correctness.cpp
+++ test/correctness.cpp
@@ -2,9 +2,20 @@
 
 #include <piwcsprwmodel.h>
 
+#include "../src/correctness.h"
+
 using namespace piwcs::prw;
 
-TEST(Correctness, Basic) {
+TEST(LocalCorrectness, BidirectionalENDIsCorrect){
+	Model model;
+	model.newNode(END, "n1");
+	model.newNode(END, "n2");
+	model.newSection("s1", Section::AllowedTravel::NONE, nullptr);
+	model.link("s1", "n1", 0, "n2", 0);
+	ASSERT_TRUE(isLocallyCorrect(model, "n1"));
+}
+
+TEST(Correctness, Basic){
 	Model model;
     model.newNode(THRU, "n1");
     model.newNode(THRU, "n2");
