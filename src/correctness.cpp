d3d4ade1d649bb73d87c8ea077680c4789cc50ac
Roman Lipov <lipov.ra@phystech.edu>
2024-05-17 16:56:14 +0300

    Revert changes rejected by maintainer in https://github.com/OLEGSHA/PiwcsPrwModel/pull/1#discussion_r1597442948
    Revert commit 574bd5a39fc3273895762c5b3d6b2128937c9202 except changes to tests.

--- src/correctness.cpp
+++ src/correctness.cpp
@@ -6,8 +6,6 @@
 
 namespace piwcs::prw {
 
-class EmptySlotError {};
-
 bool isLocallyCorrect(const Model &model, IdRef id) {
     const Node *node = model.node(id);
 
@@ -17,84 +15,76 @@ bool isLocallyCorrect(const Model &model, IdRef id) {
     isOutward.fill(false);
 
     auto sectionAt = [&](SlotId slot) -> const Section * {
-        const Section *ptr = model.section(node->section(slot));
-        if (!ptr) {
-            throw EmptySlotError();
-        }
-        return ptr;
+        return model.section(node->section(slot));
     };
 
-    try {
-        if (node->type() == END) {
-            // Single slot of end must be connected to bidir. section or
-            // forbidden section
-            return sectionAt(0)->dir() != Section::AllowedTravel::UNIDIR;
-        }
-
-        else {
-            // Set outward/inward flags
-            for (SlotId start = 0; start < node->sectionCount(); ++start) {
-                for (SlotId end = 0; end < node->sectionCount(); ++end) {
-
-                    bool isTraversable = node->couldTraverse(start, end);
-                    bool isBidirectional = sectionAt(start)->isBidir();
-                    bool isUnidirectional = sectionAt(start)->isUnidir();
-                    bool isSectionEnd = sectionAt(start)->end() == id;
-
-                    if (isTraversable && (isBidirectional ||
-                                          (isUnidirectional && isSectionEnd))) {
-                        isInward[start] = true;
-                        isOutward[end] = true;
-                    } // if
-                } // for end
-            } // for start
-
-            // Check slot correctness
-            for (SlotId slot = 0; slot < node->sectionCount(); ++slot) {
-                const Section *section = sectionAt(slot);
-
-                // Bidirectional slot
-                if (isInward[slot] && isOutward[slot]) {
-                    if (!section->isBidir()) {
-                        return false;
-                    }
-                    continue;
-                }
-
-                // None slot
-                if (!(isInward[slot] || isOutward[slot])) {
-                    if (section->allowsTravel()) {
-                        return false;
-                    }
-                    continue;
-                }
-
-                // Unidirectional slot
-                if (!section->isUnidir()) {
-                    return false;
-                }
-
-                bool isUnidirectional = section->isUnidir();
-                bool isEnd = section->end() == id;
-
-                if (!isUnidirectional) {
-                    return false;
-                }
-
-                if (isInward[slot] && !isEnd) {
-                    return false;
-                }
-
-                if (isOutward[slot] && isEnd) {
-                    return false;
-                }
-
-            } // for slot
-
-        } // else
-    } catch (const EmptySlotError &err) {
-        return false;
+    if (node->type() == END) {
+        // Single slot of end must be connected to bidir. section or forbidden
+        // section
+        return sectionAt(0)->dir() != Section::AllowedTravel::UNIDIR;
     }
+
+    else {
+        // Set outward/inward flags
+        for (SlotId start = 0; start < node->sectionCount(); ++start) {
+            for (SlotId end = 0; end < node->sectionCount(); ++end) {
+
+                bool isTraversable = node->couldTraverse(start, end);
+                bool isBidirectional = sectionAt(start)->isBidir();
+                bool isUnidirectional = sectionAt(start)->isUnidir();
+                bool isSectionEnd = sectionAt(start)->end() == id;
+
+                if (isTraversable &&
+                    (isBidirectional || (isUnidirectional && isSectionEnd))) {
+                    isInward[start] = true;
+                    isOutward[end] = true;
+                } // if
+            } // for end
+        } // for start
+
+        // Check slot correctness
+        for (SlotId slot = 0; slot < node->sectionCount(); ++slot) {
+            const Section *section = sectionAt(slot);
+
+            // Bidirectional slot
+            if (isInward[slot] && isOutward[slot]) {
+                if (!section->isBidir()) {
+                    return false;
+                }
+                continue;
+            }
+
+            // None slot
+            if (!(isInward[slot] || isOutward[slot])) {
+                if (section->allowsTravel()) {
+                    return false;
+                }
+                continue;
+            }
+
+            // Unidirectional slot
+            if (!section->isUnidir()) {
+                return false;
+            }
+
+            bool isUnidirectional = section->isUnidir();
+            bool isEnd = section->end() == id;
+
+            if (!isUnidirectional) {
+                return false;
+            }
+
+            if (isInward[slot] && !isEnd) {
+                return false;
+            }
+
+            if (isOutward[slot] && isEnd) {
+                return false;
+            }
+
+        } // for slot
+
+    } // else
 
     return true;
 }
